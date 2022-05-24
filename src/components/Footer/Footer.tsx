8869afe664ddaf67b1cade86de8ec5bcb5304694
Stepanishin <stepanishin92@mail.ru>
2022-05-24 19:45:51 +0200

    v_0.54

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ const Footer:FC = () => {
     return (
         <footer className='Footer'>
             <a className='Footer_link' href="https://github.com/Stepanishin/Solana-game" target="_blank" rel="noreferrer">Github</a>
+            <a className='Footer_link' href="https://twitter.com/Evgenii33952894" target="_blank" rel="noreferrer">Twitter</a>
         </footer>
     );
 };
